bed522e52e9da7b15f0c85eb28e3b1311832dd07
Vardhanam <vardhanam.daga@gmail.com>
2023-10-08 20:44:40 +0530

    Adding all the files for first time

---
+++ myapp/models.py
@@ -0,0 +1,13 @@
+from django.db import models
+
+# Create your models here.
+
+class Reservation(models.Model):
+    first_name= models.CharField(max_length=100)
+    last_name= models.CharField(max_length=100)
+    booking_time= models.DateTimeField(auto_now=True)
+
+    def __str__(self):
+        return self.name
+
+       
